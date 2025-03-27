3ebd8872fa1332a553c9932b286c2ad15d529f97
Jonathan <j.sweet90@hotmail.com>
2025-03-27 16:28:46 -0500

    Create home module for building Home tab
    This took a bit of research to figure out how to do. I
    grabbed svg-inline-loader because I couldn't get my svg file
    to load once removing the directly created HTML files and
    building it out with Javascript. After some searching, I saw
    that you needed the svg-inline-loader to do something like that,
    but it still didn't seem to want to appear. Well the reason was this:
    When you import the svg file in Javascript, that import is just a path
    to the SVG file. So to properly create the SVG for HTML, you build
    out your img element, but when you set the attribute for src, it needs
    to be what you named the svg's import! After that, it's good!

---
+++ src/home.js
@@ -0,0 +1,63 @@
+import coffeeSVGIcon from "./coffee-break-svgrepo-com.svg";
+
+const contentElement = document.querySelector("#content");
+
+const nameBuilder = () => {
+    const businessName = document.createElement("div");
+    businessName.textContent = "JazzHouse Coffee Co. Llc.";
+    businessName.setAttribute("class", "bus-name");
+    contentElement.appendChild(businessName);
+}
+
+const quoteBuilder = () => {
+    const quote = document.createElement("div");
+    quote.textContent = "The smoothest brew your tastebuds can enjoy.";
+    quote.setAttribute("class", "quote");
+    contentElement.appendChild(quote);
+}
+
+const contactInfoBuilder = () => {
+    const contactInfo = document.createElement("div");
+    contactInfo.setAttribute("class", "contact-info");
+    contactInfo.appendChild(numberBuilder());
+    contactInfo.appendChild(addressBuilder());
+    contentElement.appendChild(contactInfo);
+}
+
+const numberBuilder = () => {
+    const number = document.createElement("div");
+    number.textContent = "(###)###-####";
+    number.setAttribute("class", "number");
+    return number;
+}
+
+const addressBuilder = () => {
+    const address = document.createElement("div");
+    address.textContent = "111 Coffee St.";
+    address.setAttribute("class", "address");
+    return address;
+}
+
+const iconBuilder = () => {
+    const icon = document.createElement("div");
+    icon.setAttribute("class", "icon");
+    icon.appendChild(coffeeSVGBuilder());
+    contentElement.appendChild(icon);
+}
+
+const coffeeSVGBuilder = () => {
+    const coffeeSVG = document.createElement("img");
+    coffeeSVG.setAttribute("class", "coffee");
+    coffeeSVG.setAttribute("src", coffeeSVGIcon)
+    coffeeSVG.setAttribute("alt", "Picture of a coffee cup");
+    return coffeeSVG;
+}
+
+const homeTabBuilder = () => {
+    nameBuilder();
+    quoteBuilder();
+    contactInfoBuilder();
+    iconBuilder();
+}
+
+export { homeTabBuilder };
