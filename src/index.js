d16f881fec794bc14a772e2ae956b523d9dab756
Jonathan <j.sweet90@hotmail.com>
2025-03-28 16:18:10 -0500

    Add home and menu button click events building out each tab

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import "./styles.css";
 import { homeTabBuilder } from "./home";
 import { menuBuilder } from "./menu";
 
-//homeTabBuilder();
-menuBuilder();
+const contentElement = document.querySelector("#content");
+
+const homeTab = document.querySelector("#home");
+const menuTab = document.querySelector("#menu");
+
+homeTab.addEventListener("click", () => {
+    clearContent();
+    homeTabBuilder();
+});
+
+menuTab.addEventListener("click", () => {
+    clearContent();
+    menuBuilder();
+});
+
+function clearContent() {
+    while(contentElement.hasChildNodes()) {
+        contentElement.firstElementChild.remove();
+    }
+}
+
+homeTabBuilder();
