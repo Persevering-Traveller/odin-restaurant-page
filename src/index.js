52dc477241376dbabd117fb0c64198dff251389e
Jonathan <j.sweet90@hotmail.com>
2025-03-29 07:27:37 -0500

    Add aboutTab button with click event listener to build the about section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import { homeBuilder } from "./home";
 import { menuBuilder } from "./menu";
+import { aboutBuilder } from "./about";
 
 const contentElement = document.querySelector("#content");
 
 const homeTab = document.querySelector("#home");
 const menuTab = document.querySelector("#menu");
+const aboutTab = document.querySelector("#about");
 
 homeTab.addEventListener("click", () => {
     clearContent();
@@ -16,6 +18,11 @@ menuTab.addEventListener("click", () => {
     clearContent();
     menuBuilder();
 });
+
+aboutTab.addEventListener("click", () => {
+    clearContent();
+    aboutBuilder();
+})
 
 function clearContent() {
     while(contentElement.hasChildNodes()) {
